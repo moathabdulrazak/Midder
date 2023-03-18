81a83a428d7a21c7e8f2ae3554b1d8d30b42df70
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-18 04:34:02 -0600

    uploading form works... without firebase just yet

--- back2node.client/src/components/UploadSample.vue
+++ back2node.client/src/components/UploadSample.vue
@@ -1,14 +1,87 @@
 <template>
-  <h1>hhh</h1>
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+    upload
+  </button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <form @submit.prevent="createSample">
+            <div class="modal-body">
+              <div class="form-floating mb-3 elevation-5">
+                <input v-model="editable.name" type="text" required class="form-control" id="title"
+                  placeholder="event name">
+                <label for="name">sample name</label>
+              </div>
+              <div class="form-floating mb-3 elevation-5">
+                <input v-model="editable.key" type="text" required class="form-control" id="key" placeholder="location">
+                <label for="key">key</label>
+              </div>
+              <div class="form-floating mb-3 elevation-5">
+                <input v-model="editable.description" type="text" required class="form-control" id="description"
+                  placeholder="description?">
+                <label for="description">description</label>
+              </div>
+              <div class="form-floating mb-3 elevation-5">
+                <input v-model="editable.genre" type="text" required class="form-control" id="genre" placeholder="genre">
+                <label for="genre">genre</label>
+              </div>
+              <div class="form-floating mb-3 elevation-5">
+                <input v-model="editable.coverImg" type="url" required class="form-control" id="coverImg"
+                  placeholder="Cover Image">
+                <label for="coverImg">Cover Image</label>
+              </div>
+              <div class="form-floating mb-3 elevation-5">
+                <input v-model="editable.sampleUrl" type="url" required class="form-control" id="sample"
+                  placeholder="sample link">
+                <label for="sampleUrl">sampleUrl Image</label>
+              </div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+              <button type="submit" class="btn btn-primary">Save changes</button>
+            </div>
+          </form>
+
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <script>
 import { AppState } from '../AppState';
-import { computed, reactive, onMounted } from 'vue';
+import { computed, reactive, onMounted, ref } from 'vue';
+import Pop from "../utils/Pop.js";
+import { Modal } from "bootstrap";
+import { sampleService } from "../services/SamplesService.js";
 export default {
   setup() {
-    return {}
+    const editable = ref({})
+    return {
+      editable,
+      async createSample() {
+        try {
+          const sample = await sampleService.createSample(editable.value)
+          editable.value = {}
+          Modal.getOrCreateInstance('#exampleModal').hide()
+        } catch (error) {
+          Pop.error(error)
+        }
+      }
+    }
   }
 };
 </script>
