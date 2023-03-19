aa3c6760abbed74b50c7c02eaa40a3aa2908e1d6
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-19 04:21:08 -0600

    border radius on the account photo

--- back2node.client/src/components/Login.vue
+++ back2node.client/src/components/Login.vue
@@ -5,11 +5,10 @@
       Login
     </button>
     <div v-else>
-      <div class="dropdown dropstart my-2 my-lg-0">
-        <div type="button" class="bg-dark border-0 selectable no-select" data-bs-toggle="dropdown"
-          aria-expanded="false">
+      <div class="dropdown rounded-vfx dropstart my-2 my-lg-0 vfx">
+        <div type="button" class=" rounded-vfx selectable " data-bs-toggle="dropdown" aria-expanded="false">
           <div v-if="account.picture || user.picture">
-            <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded" />
+            <img :src="account.picture || user.picture" alt="account photo" height="40" class="rounded-vfx" />
           </div>
         </div>
         <div class="dropdown-menu dropdown-menu-lg-left p-0" aria-labelledby="authDropdown">
@@ -51,4 +50,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.rounded-vfx {
+  border-radius: 50%;
+}
 </style>
