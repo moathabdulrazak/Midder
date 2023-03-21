f9805d8c8523536d3f6a9f7e305a99787882295a
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-20 23:44:31 -0600

    followers model

---
+++ back2node/server/models/Follower.js
@@ -0,0 +1,24 @@
+import { Schema } from "mongoose";
+
+
+
+
+
+
+
+
+export const FollowerSchema = new Schema({
+  accountId: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
+  followingId: { type: Schema.Types.ObjectId, required: true, ref: "Account" },
+}, { timestamps: true, toJSON: { virtuals: true } })
+
+
+
+FollowerSchema.virtual('creator', {
+  localField: 'followingId',
+  ref: "Account",
+  foreignField: '_id',
+  justOne: true
+})
+
+FollowerSchema.index({ followingId: 1, accountId: 1 }, { unique: true })
