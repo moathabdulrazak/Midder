6531eb6e7584f097c6bb99458a1303d8a8f4d935
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-23 02:17:25 -0600

    starting on profile page..

--- back2node.client/src/pages/ProfilePage.vue
+++ back2node.client/src/pages/ProfilePage.vue
@@ -9,8 +9,29 @@
 <script>
 import { AppState } from '../AppState';
 import { computed, reactive, onMounted } from 'vue';
+import { useRoute } from "vue-router";
+import Pop from "../utils/Pop.js";
+import { profileService } from "../services/ProfilesService.js";
 export default {
   setup() {
+    const route = useRoute();
+
+
+
+
+    async function getProfile() {
+      try {
+        await profileService.getProfile(route.params.profileId)
+      } catch (error) {
+        Pop, error(error)
+      }
+    }
+
+
+    onMounted(() => {
+      getProfile()
+    })
+
     return {}
   }
 };
