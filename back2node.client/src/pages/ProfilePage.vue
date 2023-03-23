d604339bae46d372bc51d80c33f7a0a3c399c6e2
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-23 02:27:39 -0600

    basic layout

--- back2node.client/src/pages/ProfilePage.vue
+++ back2node.client/src/pages/ProfilePage.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="component">
-    profile page
+  <div class="container-fluid">
+    <div class="row container-fluid">
+      <div class="row container-fluid">
+      </div>
+    </div>
+    <div>
+      <div v-if="profile" class="row container-fluid cover-img" :style="`background-image: url(${profile.coverImg})`">
+        <div class="col-12 d-flex align-items-center justify-content-around">
+          <img :src="profile.picture" alt="" class="img-fluid profile-picture rounded-circle elevation-5">
+          <div class="bg-transparent rounded elevation-5 p-5">
+            <h1 class="elevation-1">
+              {{ profile.name }}
+            </h1>
+            <h2>
+              {{ profile.bio }}
+            </h2>
 
+          </div>
+        </div>
+      </div>
+
+
+    </div>
   </div>
 </template>
 
@@ -32,7 +52,9 @@ export default {
       getProfile()
     })
 
-    return {}
+    return {
+      profile: computed(() => AppState.activeProfile),
+    }
   }
 };
 </script>
