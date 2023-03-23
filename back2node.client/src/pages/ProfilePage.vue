07e1ec93a50e9cb03d3faab5b26193fd7fbf7bc0
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-23 02:29:26 -0600

    styling

--- back2node.client/src/pages/ProfilePage.vue
+++ back2node.client/src/pages/ProfilePage.vue
@@ -7,7 +7,7 @@
     <div>
       <div v-if="profile" class="row container-fluid cover-img" :style="`background-image: url(${profile.coverImg})`">
         <div class="col-12 d-flex align-items-center justify-content-around">
-          <img :src="profile.picture" alt="" class="img-fluid profile-picture rounded-circle elevation-5">
+          <img :src="profile.picture" alt="" class="img-fluid profile-picture rounded-circle elevation-5 slide-in">
           <div class="bg-transparent rounded elevation-5 p-5">
             <h1 class="elevation-1">
               {{ profile.name }}
@@ -19,11 +19,10 @@
           </div>
         </div>
       </div>
-
-
     </div>
   </div>
 </template>
+
 
 
 <script>
@@ -60,4 +59,64 @@ export default {
 </script>
 
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+/* Set up the cover image */
+.cover-img {
+  height: 300px;
+  background-position: center;
+  background-size: cover;
+}
+
+/* Style the profile picture */
+.profile-picture {
+  width: 200px;
+  height: 200px;
+  border: 5px solid white;
+  transition: all 0.2s ease-in-out;
+}
+
+/* Add a hover effect to the profile picture */
+.profile-picture:hover {
+  transform: scale(1.1);
+}
+
+/* Style the profile name and bio */
+h1,
+h2 {
+  margin: 0;
+}
+
+h1 {
+  font-size: 3rem;
+  font-weight: bold;
+  color: #333;
+}
+
+h2 {
+  font-size: 2rem;
+  font-weight: normal;
+  color: #666;
+}
+
+/* Add some elevation to the profile information */
+.elevation-5 {
+  box-shadow: 0px 3px 3px rgba(0, 0, 0, 0.1);
+}
+
+/* Add a cool slide-in animation to the profile picture */
+.profile-picture.slide-in {
+  animation: slide-in 0.5s ease-in-out forwards;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+
+  to {
+    transform: translateX(0);
+    opacity: 1;
+  }
+}
+</style>
