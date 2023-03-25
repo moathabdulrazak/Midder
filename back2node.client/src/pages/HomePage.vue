965d0445fea0aebf1680698197b51a78f6272eef
moath abdulrazak <moathabdulrazak12@gmail.com>
2023-03-25 02:31:02 -0600

    working on filtering, search system

--- back2node.client/src/pages/HomePage.vue
+++ back2node.client/src/pages/HomePage.vue
@@ -1,41 +1,102 @@
 <template>
   <div class="row justify-content-center pt-2 container-fluid">
-
-    <div class="col-11" v-for="s in samples">
+    <div class="col-12 col-md-4 pb-3">
+      <div class="input-group">
+        <input type="text" class="form-control" placeholder="Search for users or samples" v-model="search">
+        <div class="input-group-append">
+          <button class="btn btn-outline-secondary mdi" type="button" @click="searchSamples"><i
+              class="fa fa-search"></i></button>
+        </div>
+      </div>
+    </div>
+    <div class="col-12 col-md-4 pb-3">
+      <div class="dropdown">
+        <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown"
+          aria-haspopup="true" aria-expanded="false">
+          Filter by genre
+        </button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a class="dropdown-item" href="#" @click="setGenre('all')">All</a>
+          <a class="dropdown-item" href="#" @click="setGenre('hiphop')">Hip Hop</a>
+          <a class="dropdown-item" href="#" @click="setGenre('rock')">Rock</a>
+          <a class="dropdown-item" href="#" @click="setGenre('electronic')">Electronic</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-11" v-for="s in filteredSamples">
       <SamplesCard :sample="s" />
-
     </div>
   </div>
 </template>
 
 <script>
-import { async } from "@firebase/util";
-import { computed, onMounted } from "vue";
+import { computed, onMounted, ref } from "vue";
 import { AppState } from "../AppState.js";
 import { sampleService } from "../services/SamplesService.js";
 import { logger } from "../utils/Logger.js";
-import SamplesCard from "../components/SamplesCard.vue"
+import SamplesCard from "../components/SamplesCard.vue";
 import Pop from "../utils/Pop.js";
 
 export default {
+  components: {
+    SamplesCard,
+  },
   setup() {
+    const search = ref("");
+    const genre = ref("all");
+
     async function getAllSamples() {
       try {
-        await sampleService.getAllSamples();
-      }
-      catch (error) {
+        const samples = await sampleService.getAllSamples();
+      } catch (error) {
         logger.log(error.message);
         Pop.error(error.message);
       }
     }
+
+    function setGenre(selectedGenre) {
+      genre.value = selectedGenre;
+    }
+
+    function filterSamples(samples, genre, search) {
+      if (!samples) {
+        return [];
+      }
+
+      return samples.filter(sample => {
+        if (genre !== "all" && sample.genre !== genre) {
+          return false;
+        }
+        if (search && !sample.name.toLowerCase().includes(search.toLowerCase()) && !sample.creator.name.toLowerCase().includes(search.toLowerCase())) {
+          return false;
+        }
+        return true;
+      });
+    }
+
+    function searchSamples() {
+      AppState.samples = filterSamples(
+        AppState.samples,
+        genre.value,
+        search.value
+      );
+    }
+
     onMounted(() => {
       getAllSamples();
     });
+
     return {
-      samples: computed(() => AppState.samples),
+      search,
+      genre,
+      filteredSamples: computed(() =>
+        filterSamples(AppState.samples, genre.value, search.value)
+      ),
+      setGenre,
+      searchSamples,
     };
   },
-}
+};
 </script>
 
 <style scoped lang="scss">
